f518e1fe1ce35dc584184fa5f5910ee4dc746373
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-03-31 22:10:17 -0400

    Added cors

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationsController < ApplicationController
+
+    require 'sendgrid-ruby'
+    include SendGrid
+
+    def create
+        send_message
+        render json: {message: "Hi"}
+    end
+
+    def send_message
+        from = SendGrid::Email.new(email: ENV['SENDER_ADDRESS'])
+        to = SendGrid::Email.new(email: ENV['RECIPIENT_ADDRESS'])
+        subject = 'Someone submitted an application'
+        content = SendGrid::Content.new(type: 'text/plain', value: 'Check out this application')
+        mail = SendGrid::Mail.new(from, subject, to, content)
+
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+        puts response
+    end
+end
