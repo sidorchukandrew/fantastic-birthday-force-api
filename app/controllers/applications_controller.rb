7376222cf565ea6d740de958eeaa44662e37c40b
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-03-31 23:20:55 -0400

    Added dynamic templates to SendGrid

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -9,14 +9,20 @@ class ApplicationsController < ApplicationController
     end
 
     def send_message
-        from = SendGrid::Email.new(email: ENV['SENDER_ADDRESS'])
-        to = SendGrid::Email.new(email: ENV['RECIPIENT_ADDRESS'])
-        subject = "Someone submitted an application for #{params[:position]}"
-        content = SendGrid::Content.new(type: 'text/plain', value: 'Check out this application')
-        mail = SendGrid::Mail.new(from, subject, to, content)
+        mail = SendGrid::Mail.new
+        mail.from = SendGrid::Email.new(email: ENV['SENDER_ADDRESS'])
+        personalization = SendGrid::Personalization.new
+        personalization.add_to(SendGrid::Email.new(email: 'andrew@aimsparking.com'))
+        personalization.add_dynamic_template_data(application_params.merge({subject: "#{application_params[:position]} | New Application"}))
+        mail.add_personalization(personalization)
+        mail.template_id = ENV['SENDGRID_TEMPLATE_ID']
 
         sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-        response = sg.client.mail._('send').post(request_body: mail.to_json)
-        puts response
+        response = sg.client.mail._("send").post(request_body: mail.to_json)
+    end
+
+    private
+    def application_params
+        params.require(:application).permit!
     end
 end
